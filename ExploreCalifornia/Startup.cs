5bc3aa5d2427499d3b2ea4590638e746025892c4
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-07-14 19:18:58 +0200

    pridanie error handlingu

--- ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ExploreCalifornia
 {
@@ -18,26 +19,46 @@ namespace ExploreCalifornia
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); //zabezpecuje zobrazenie developerskej error page
+            }
+            else
+            {
+                app.UseExceptionHandler("/error.html");
             }
 
-            app.Use(async (context, next) => //Use + next --> definovania, ze ma byt po vykonani tohto middleware vykonany aj nasledujuci
+            app.Use(async(context, next) =>
             {
-                if (context.Request.Path.Value.StartsWith("/hello")) //middleware vykonany len pri urcitej URL
+                if (context.Request.Path.Value.StartsWith("/invalid"))
                 {
-                    await context.Response.WriteAsync("Hello ASP.NET Core!");
+                    throw new Exception("Error");
                 }
-                await next();
-            });
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("How are you?");
-            });
+                await next();
+            }); 
+
+            app.UseFileServer(); //middleware sa pokusi zadanu URL previest na subor v wwwroot lokacii
+            //wwwroot --> specialny folder oddelujuci staticke subory, ktore maju byt k dispozicii pre uzivatelov aplikacie od zvysku aplikacneho kodu
+
+
+            //app.Use(async (context, next) => //Use + next --> definovania, ze ma byt po vykonani tohto middleware vykonany aj nasledujuci
+            //{
+            //    if (context.Request.Path.Value.StartsWith("/hello")) //middleware vykonany len pri urcitej URL
+            //    {
+            //        await context.Response.WriteAsync("Hello ASP.NET Core!");
+            //    }
+            //    await next();
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("How are you?");
+            //});
         }
     }
 }
