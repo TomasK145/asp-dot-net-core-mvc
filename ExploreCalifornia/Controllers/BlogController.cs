7439f71c5e29d56fa4ff5f48c630c6c684d4b196
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-07-15 15:45:16 +0200

    4. work with data --> vytvorenie formu pre moznost vytvarat zaznamy, POST, pridanie validacie + validationsummary, pouzitie TagHelpers, wip

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -14,9 +14,26 @@ namespace ExploreCalifornia.Controllers
         //ak je definovany route v atribute pre action, jediny sposob ako action vykonat je matchnut definovanu route v atribute
         public IActionResult Index()
         {
-            return View();
+            var posts = new[]
+            {
+                new Post
+                {
+                    Title = "My blog post",
+                    Posted = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is a great blog post, don't you think?",
+                },
+                new Post
+                {
+                    Title = "My second blog post",
+                    Posted = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is ANOTHER great blog post, don't you think?",
+                },
+            };
+            return View(posts);
         }
-        
+
         [Route("{year:int}/{month:range(1,12)}/{key}")] //--> vyuzitie custom route pre action
         //public IActionResult Post(string id) //parameter ziuskany z query string (?Id=123) alebo Url (.../post/123) --> model binding ;;; mozno vyuzit nullable premenne alebo s default values
         public IActionResult Post(int year, int month, string key)
@@ -29,6 +46,28 @@ namespace ExploreCalifornia.Controllers
                 Body = "This is a great blog post, don't you think?"
             };
             return View(post);
+        }
+
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("create")]
+        //public IActionResult Create([Bind("Title", "Body")]Post post) //--> Bind definuje ktore properties mozu byt naplnene uzivatelom z formu
+        public IActionResult Create(Post post) //mozne namiesto Post pouzit novy objekt (pr. CreatePostRequest), ktory obsahuje len property naplnene vo forme 
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            //popr je mozne overridnut hodnoty pre properties ktore mohol uzivatel zadat, no nemali by byt samotnym uzivatelom menene
+            post.Author = User.Identity.Name;
+            post.Posted = DateTime.Now;
+
+            return View();
         }
     }
 }
--- ExploreCalifornia/Models/POst.cs
+++ ExploreCalifornia/Models/POst.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        [Display(Name = "Post tile")]
+        [DataType(DataType.Text)]
+        [Required]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 100 characters long")]
         public string Title { get; set; }
         public DateTime Posted { get; set; }
         public string Author { get; set; }
+        [Required]
+        [MinLength(100, ErrorMessage = "Blog posts must be at least 100 characters long")]
         public string Body { get; set; }
     }
 }
